fa1f567833c8f018050aa03ede7f3255ddb92cbb
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-04-04 14:41:07 +0100

    Create transferencia.h

---
+++ include/transferencia.h
@@ -0,0 +1,13 @@
+//
+// Created by pablo on 04/04/2026.
+//
+
+#ifndef EDAP1_TRANSFERENCIA_H
+#define EDAP1_TRANSFERENCIA_H
+#include "../include/jogador.h"
+
+Jogador** gerarTransferencia(Jogador* aleatorios);
+void imprimirListaTransferencias(Jogador** listaTransferencia, int jornadaAtual);
+
+
+#endif //EDAP1_TRANSFERENCIA_H
