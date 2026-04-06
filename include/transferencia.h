efa2bdf083fe4112c7bdf365ca76015ef249a509
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-04-06 15:49:36 +0100

    Update transferencia.h

--- include/transferencia.h
+++ include/transferencia.h
@@ -5,9 +5,20 @@
 #ifndef EDAP1_TRANSFERENCIA_H
 #define EDAP1_TRANSFERENCIA_H
 #include "../include/jogador.h"
+#include "../include/equipa.h"
 
-Jogador** gerarTransferencia(Jogador* aleatorios);
-void imprimirListaTransferencias(Jogador** listaTransferencia, int jornadaAtual);
+int posicao(std::string pos);
+int totalJogadores(Equipa &equipe);
+Jogador* gerarTransferencia(Jogador* novos, int numNovos, Jogador* listaAntiga, int &totalTransferencias);
+void imprimirMercado(Jogador* listaTransf, int totalTransf);
+bool podeAdicionar(Equipa &equipe, int pos);
+int escolherCamisaDisponivel(Equipa &equipe, int posIdx);
+void adicionarTransferencia(Jogador jogador, Jogador* &listaTransferencia, int &totalTransferencias);
+void adicionarJogadorPlantel(Equipa &equipe, Jogador jogador);
+bool removerJogadorPlantel(Equipa &equipe, Jogador* &listaTransferencia, int &totalTransferencias, int numCamisa);
+Jogador removerTransferencia(Jogador* &listaTransferencia, int &totalTransferencias, int indice);
+void contratarJogador(Equipa &equipe, Jogador* &listaTransferencia, int &totalTransferencias);
+
 
 
 #endif //EDAP1_TRANSFERENCIA_H
