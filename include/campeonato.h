84551470d6f82b0517a65883a8ef1abe5d1ea109
pablosilvabr <pablo.a.silva.pt@gmail.com>
2026-04-08 22:22:39 +0100

    Create campeonato.h

---
+++ include/campeonato.h
@@ -0,0 +1,11 @@
+//
+// Created by pablo on 08/04/2026.
+//
+
+#ifndef EDAP1_CAMPEONATO_H
+#define EDAP1_CAMPEONATO_H
+#include "../include/equipa.h"
+
+void tabelaPontos (Equipa* principal, Equipa* adversarios);
+
+#endif //EDAP1_CAMPEONATO_H
